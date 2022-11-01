5f14c0e4652934088b1c93a9218f9d5300d51c4e
mustafaulker <mcubjk@gmail.com>
2022-11-01 17:36:37 +0300

    Create HR Signup page Gives staff permissions to the HR account

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, HRSignUpView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('hr_signup/', HRSignUpView.as_view(), name='hr_signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group
 from django.urls import reverse_lazy
 from django.views import generic
+
+applicant_group, created = Group.objects.get_or_create(name="Applicant")
+hr_group, created = Group.objects.get_or_create(name="HR")
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = "registration/signup.html"
+
+    def form_valid(self, form):
+        user = form.save()
+        applicant_group.user_set.add(user)
+        return super().form_valid(form)
+
+
+class HRSignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = "registration/hr_signup.html"
+
+    def form_valid(self, form):
+        form.instance.is_staff = True
+        user = form.save()
+        hr_group.user_set.add(user)
+        return super().form_valid(form)
